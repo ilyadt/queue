1c8018f833157dbf3c70c66117c0dd78f11cfdb0
Ilya D <ilya_dt@protonmail.com>
2023-04-08 21:51:49 +0600

    put value without queue name

--- tests/features/validation.feature
+++ tests/features/validation.feature
@@ -3,3 +3,7 @@ Feature: Validation constraints
     Scenario: Get value with negative timeout
         When I request value with negative timeout
         Then I get 400 status
+
+    Scenario: Put value without queue name
+        When I put value without queue name
+        Then I get 400 status
--- tests/validation/s1_test.go
+++ tests/validation/s1_test.go
@@ -30,6 +30,18 @@ func iRequestValueWithNegativeTimeout(ctx context.Context) (context.Context, err
 	return context.WithValue(ctx, "response", resp), nil
 }
 
+func iPutValueWithoutQueueName(ctx context.Context) (context.Context, error) {
+	baseUrl := ctx.Value("serverBaseURL").(string)
+
+	req, _ := http.NewRequest("PUT", baseUrl + "/?value=red", nil)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return ctx, fmt.Errorf("put request failed to perform: %w", err)
+	}
+
+	return context.WithValue(ctx, "response", resp), nil
+}
 
 func InitializeScenario(ctx *godog.ScenarioContext) {
 	ctx.Before(func(ctx context.Context, sc *godog.Scenario) (context.Context, error) {
@@ -38,7 +50,7 @@ func InitializeScenario(ctx *godog.ScenarioContext) {
 
 	ctx.Step(`^I get (\d+) status$`, iGetStatus)
 	ctx.Step(`^I request value with negative timeout$`, iRequestValueWithNegativeTimeout)
-
+	ctx.Step(`^I put value without queue name$`, iPutValueWithoutQueueName)
 }
 
 func TestFeatures(t *testing.T) {
